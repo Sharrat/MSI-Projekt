3bd12e3bdbfa116bd882d94ada121ac3b0a148b3
Sharrat <mrkaczucha@gmail.com>
2023-04-02 22:31:24 +0200

    Update parametrów

--- Skrypty/Eksperymenty.py
+++ Skrypty/Eksperymenty.py
@@ -5,12 +5,12 @@ from strlearn.evaluators import TestThenTrain
 from incremental_kmeans import IncrementalKmeans
 from sklearn.metrics import normalized_mutual_info_score
 
-strumien = StreamGenerator(random_state=42, n_chunks=100, chunk_size=100, n_classes=2, n_features=10, n_informative=2,
-n_redundant=0, n_clusters_per_class=2)
+strumien = StreamGenerator(random_state=42, n_chunks=100, chunk_size=100, n_classes=3, n_features=10, n_informative=2,
+n_redundant=0, n_clusters_per_class=1)
 
 metryki = [adjusted_rand_score, normalized_mutual_info_score]
 # Inicjalizacja modelu Incremental KMeans
-model = IncrementalKmeans(random_state=42, n_clusters=2)
+model = IncrementalKmeans(random_state=42, n_clusters=3)
 # Inicjalizacja ewaluatora i dopasowanie modelu do strumienia danych
 ewaluator = TestThenTrain(metryki)
 ewaluator.process(strumien, model)
